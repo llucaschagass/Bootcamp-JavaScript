d74bb08f2e910b389bd285b745bde5fe51bedd15
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2024-11-07 22:38:19 -0300

    feat: entregando desafio de projeto Github Wiki

---
+++ XP - Full Stack Developer/Desafios de Projeto/Github-Wiki/src/components/Button/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+
+    background-color: #000;
+    color: #FFFFFF;
+    border: 1px solid #00FF00;
+    border-radius: 20px;
+    font-size: 20px;
+    font-family: 'Roboto';
+
+    height: 62px;
+    width: 15%;
+
+    margin: 10px;
+    }
+
+    &: hover {
+        background-color: #FAFAFA80;
+        cursor: pointer;
+    }
+`;
---
+++ XP - Full Stack Developer/Desafios de Projeto/Github-Wiki/src/components/Input/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+
+    border: 1px solid #FAFAFA;
+    border-radius: 20px;
+
+    height: 62px;
+    width: 50%;
+
+    margin: 20px;
+
+    input {
+        background: transparent;
+        border: 0;
+        border-radius: 20px;
+        width: 95%;
+        height: 62px;
+        padding: 0 20px;
+        color: #FFFFFF;
+        font-size: 18px;
+    }
+`
